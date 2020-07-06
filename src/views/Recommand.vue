c877a8201fc5b47595640b0bba1d96a8e034b629
felixeasyfly <59648572+felixeasyfly@users.noreply.github.com>
2020-07-06 15:58:43 +0800

    Add files via upload

--- src/views/Recommand.vue
+++ src/views/Recommand.vue
@@ -8,7 +8,100 @@
     </el-menu-item>
 </el-menu>
   </el-header>
-  <el-main>施工中！</el-main>
+  <el-main>
+    <el-row>
+      <el-col :span="24">
+         
+         <div style="margin-top: 20px" class="sortradiogroup"> 
+           <span>电影类型  </span>
+      <el-radio-group v-model="tagradio" size="small">
+        <el-radio-button label="全部类型"></el-radio-button>
+      <el-radio-button label="动作"></el-radio-button>
+      <el-radio-button label="喜剧"></el-radio-button>
+      <el-radio-button label="爱情"></el-radio-button>
+      <el-radio-button label="科幻"></el-radio-button>
+      <el-radio-button label="动画"></el-radio-button>
+      <el-radio-button label="悬疑"></el-radio-button>
+      <el-radio-button label="惊悚"></el-radio-button>
+      <el-radio-button label="恐怖"></el-radio-button>
+      <el-radio-button label="犯罪"></el-radio-button>
+      <el-radio-button label="历史"></el-radio-button>
+      <el-radio-button label="战争"></el-radio-button>
+      <el-radio-button label="西部"></el-radio-button>
+       <el-radio-button label="其他"></el-radio-button>
+    </el-radio-group>
+  </div>
+  <div style="margin-top: 20px" class="sortradiogroup">
+           <span>电影地区  </span>
+      <el-radio-group v-model="countryradio" size="small">
+        <el-radio-button label="全部地区"></el-radio-button>
+      <el-radio-button label="中国大陆"></el-radio-button>
+      <el-radio-button label="中国香港"></el-radio-button>
+      <el-radio-button label="中国台湾"></el-radio-button>
+      <el-radio-button label="美国"></el-radio-button>
+      <el-radio-button label="日本"></el-radio-button>
+      <el-radio-button label="韩国"></el-radio-button>
+      <el-radio-button label="英国"></el-radio-button>
+      <el-radio-button label="意大利"></el-radio-button>
+      <el-radio-button label="印度"></el-radio-button>
+      <el-radio-button label="法国"></el-radio-button>
+      <el-radio-button label="德国"></el-radio-button>
+      <el-radio-button label="俄罗斯"></el-radio-button>
+      <el-radio-button label="西班牙"></el-radio-button>
+      <el-radio-button label="其他"></el-radio-button>
+    </el-radio-group>
+  </div>
+   <div style="margin-top: 20px" class="sortradiogroup">
+           <span>电影年代  </span>
+      <el-radio-group v-model="dateradio" size="small">
+        <el-radio-button label="全部年代"></el-radio-button>
+        <el-radio-button label="2020"></el-radio-button>
+      <el-radio-button label="2019"></el-radio-button>
+      <el-radio-button label="2018"></el-radio-button>
+      <el-radio-button label="2017"></el-radio-button>
+      <el-radio-button label="2010年代"></el-radio-button>
+      <el-radio-button label="2000年代"></el-radio-button>
+      <el-radio-button label="90年代"></el-radio-button>
+      <el-radio-button label="80年代"></el-radio-button>
+      <el-radio-button label="70年代"></el-radio-button>
+      <el-radio-button label="60年代"></el-radio-button>
+      <el-radio-button label="更早"></el-radio-button>
+    </el-radio-group>
+  </div>
+  <div style="margin-top: 20px" class="sortradiogroup">
+           
+      <el-radio-group v-model="sortradio" size="small">
+        <el-radio-button label="近期热门"></el-radio-button>
+        <el-radio-button label="评分最高"></el-radio-button>
+        <el-radio-button label="近期上映"></el-radio-button>
+    </el-radio-group>
+  </div>
+      </el-col>
+      
+    </el-row>
+  <el-row gutter="10"> 
+    <el-divider></el-divider>           
+    <el-col :span="3" v-for="o in 10  " :key="o" offset="1">
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <span>电影名</span>
+        <span style="padding:50px;"> 评分</span>
+      </div>
+    </el-card>
+    
+  </el-col>
+		</el-row>      
+<el-row>
+   <el-divider></el-divider>       
+  <el-pagination
+  style="text-align:center;"
+  background
+  layout="prev, pager, next"
+  :total="1000">
+</el-pagination>
+</el-row>
+  </el-main>
 </el-container>
 </div>
 </template>
@@ -25,9 +118,15 @@ export default {
         navList:[
             {name:'/HomePage',navItem:'首页'},
             {name:'/HomePage/UserCenter',navItem:'个人中心'},
-            {name:'/Recommand',navItem:'个性化推荐'},
+            {name:'/Recommand',navItem:'分类查询'},
             {name:'/Comment',navItem:'电影评价'}
-        ]
+        ],
+         tagradio:'全部类型',
+         countryradio:'全部地区',
+         dateradio:'全部年代',
+         sortradio:'近期热门',
+         movies:[],
+	
       };
     },
     methods: {
@@ -40,34 +139,39 @@ export default {
 
 <style scoped>
 
-.el-header {
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-main {
-
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+.sortradiogroup{
+    margin:0 500px;
+}
 .el-menu{
     padding:0 100px;
 }
+ .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
 
+  .button {
+    padding: 0;
+    float: right;
+  }
 
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
 </style>
